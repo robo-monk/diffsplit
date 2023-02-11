06938dbc646a89d12e866c4ea4393801b9b48b1c
robo-monk <rrobomonk@gmail.com>
2023-02-11 16:42:02 +0100

    init stytles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import './styles.scss';
+	import { Button } from 'carbon-components-svelte';
+</script>
+
+<div class="flex items-center flex-col max-w-lg mx-auto">
+	<div class="my-5">
+		<h1>Diffsplit</h1>
+		<div />
+		<Button>Create a chain</Button>
+	</div>
+</div>
--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,21 @@
 import adapter from '@sveltejs/adapter-auto';
+import preprocess from 'svelte-preprocess';
 import { vitePreprocess } from '@sveltejs/kit/vite';
+import { optimizeImports as carbonImportOptim, optimizeCss as carbonCssOptim } from "carbon-preprocess-svelte";
+
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+    // Consult https://github.com/sveltejs/svelte-preprocess
+    // for more information about preprocessors
+    preprocess: [preprocess(), vitePreprocess(), carbonImportOptim()],
 
-	kit: {
-		adapter: adapter()
-	}
+    vite: {
+        plugins: [process.env.NODE_ENV === "production" && carbonCssOptim()],
+    },
+    kit: {
+        adapter: adapter(),
+    }
 };
 
 export default config;
