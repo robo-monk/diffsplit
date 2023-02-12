72d7cfa18dccc16405f0a9b15311274453355882
bang-a-coder <nikitas4717@gmail.com>
2023-02-12 01:04:02 +0100

    qr

---
+++ src/routes/invite/Invite.svelte
@@ -0,0 +1,41 @@
+<script lang='ts'>
+	import { Button } from 'carbon-components-svelte';
+
+	import QRCode from 'qrcode'
+
+	export let url: string = ''
+
+	let qrimage: HTMLImageElement;
+
+
+	const generateQR = async (url: string) => {
+		try {
+			let qrurl = await QRCode.toDataURL(url) 
+			console.log(qrurl)
+
+			qrimage.src = qrurl
+			return qrurl
+		} catch (err) {
+			console.error(err)
+		}
+	}	
+
+	generateQR(url)
+
+</script>
+
+<div class="flex items-center flex-col max-w-lg mx-auto">
+
+	<div class="qrcode">
+		<!-- svelte-ignore a11y-missing-attribute -->
+		<img src='' bind:this={qrimage}>
+	</div>
+	
+	<div class="flex flex-col">
+		<Button class="my-5">Copy URL</Button>
+		<Button kind="secondary">Share</Button>
+	</div>
+
+</div>
+
+
